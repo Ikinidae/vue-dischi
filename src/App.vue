f0906a47e9816cb061a2660e9854a0578777f868
Ikinidae <federicaelia24@gmail.com>
2022-06-10 16:59:04 +0200

    Inizio progetto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HeaderDischi/>
+    <MainDischi/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderDischi from './components/HeaderDischi.vue'
+import MainDischi from './components/MainDischi.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderDischi,
+    MainDischi
   }
 }
 </script>
