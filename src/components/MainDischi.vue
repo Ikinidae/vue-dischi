54f5fab6f9d48bc52d5d665f3c503d0af7e1b0bf
Ikinidae <federicaelia24@gmail.com>
2022-06-10 17:43:22 +0200

    Stampate card tramite API, manca style

--- src/components/MainDischi.vue
+++ src/components/MainDischi.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>Main</h1>
+  <div>
+    <MusicCard
+        v-for="(item, i) in albumList"
+        :key="i"
+        :albumObject="item"
+    />
   </div>
 </template>
 
 <script>
+import MusicCard from './MusicCard.vue'
+import axios from "axios"
+
 export default {
-  name: 'MainDischi',
+    name: "MainDischi",
+    components: {
+        MusicCard
+    },
+    data () {
+        return {
+            apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+            albumList: []
+        }
+    },
+    created () {
+        this.getAlbum();
+    },
+    methods: {
+        getAlbum () {
+            axios
+            .get(this.apiUrl)
+            .then((result) =>{
+                this.albumList = result.data.response;
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log("Errore", error);
+            })
+        }
+    }
 }
 </script>
 
---
+++ src/components/MusicCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img :src="albumObject.poster" :alt="albumObject.title">
+    <h2>{{albumObject.title}}</h2>
+    <p>{{albumObject.author}}</p>
+    <p>{{albumObject.year}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicCard',
+  props: {
+      albumObject: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
